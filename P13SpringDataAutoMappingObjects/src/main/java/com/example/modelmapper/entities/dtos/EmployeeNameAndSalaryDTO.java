786fbd7c8696c3108478c6f4849916febdbe457a
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-04-20 00:27:31 +0300

    update

---
+++ P13SpringDataAutoMappingObjects/src/main/java/com/example/modelmapper/entities/dtos/EmployeeNameAndSalaryDTO.java
@@ -0,0 +1,9 @@
+package com.example.modelmapper.entities.dtos;
+
+import java.math.BigDecimal;
+
+public interface EmployeeNameAndSalaryDTO {
+    String getFirstName();
+
+    BigDecimal getSalary();
+}
