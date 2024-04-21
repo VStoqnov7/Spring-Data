3712546c5a42571e79a168af87e51c835caf3b3d
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-04-22 00:08:02 +0300

    update

---
+++ P15JSONProcessing-ProductsShop-CarDealer/src/main/java/com/example/productsshop/enums/OutJsonPaths.java
@@ -0,0 +1,16 @@
+package com.example.productsshop.enums;
+
+public class OutJsonPaths {
+    ;
+
+    public final static String PRODUCTS_IN_RANGE = "src\\main\\resources\\output\\products-in-range.json";
+    public final static String SUCCESSFULLY_SOLD_PRODUCTS = "src\\main\\resources\\output\\users-sold-products.json";
+    public final static String CATEGORIES_BY_PRODUCTS_COUNT = "src\\main\\resources\\output\\categories-by-products.json";
+    public final static String USERS_AND_PRODUCTS = "src\\main\\resources\\output\\users-and-products.json";
+    public final static String ORDERED_CUSTOMERS = "src\\main\\resources\\output\\ordered-customers.json";
+    public final static String TOYOTA_CARS = "src\\main\\resources\\output\\toyota-cars.json";
+    public final static String LOCAL_SUPPLIERS = "src\\main\\resources\\output\\local-suppliers.json";
+    public final static String CAR_AND_PARTS = "src\\main\\resources\\output\\cars-and-parts.json";
+    public final static String CUSTOMERS_TOTAL_SALES = "src\\main\\resources\\output\\customers-total-sales.json";
+    public final static String SALES_DISCOUNTS = "src\\main\\resources\\output\\sales-discounts.json";
+}
