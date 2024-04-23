453224e521d32d179d9611682755ca51b32b04ef
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-04-24 00:13:31 +0300

    update

---
+++ P16ModelMapper_Json_Xml/src/main/java/com/example/modelmapper_json_xml/P16ModelMapperJsonXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.modelmapper_json_xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P16ModelMapperJsonXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P16ModelMapperJsonXmlApplication.class, args);
+    }
+
+}
