783ed3e2b3e24ffce6d584a0dc746a518e367b64
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-04-26 00:01:22 +0300

    update

---
+++ P17XMLProcessing-ProductShop-CarDealer/src/main/java/com/example/productshop/enums/OutXmlPaths.java
@@ -0,0 +1,15 @@
+package com.example.productshop.enums;
+
+public class OutXmlPaths {
+    ;
+    public final static String PRODUCTS_IN_RANGE = "src\\main\\resources\\output\\products-in-range.xml";
+    public final static String SUCCESSFULLY_SOLD_PRODUCTS = "src\\main\\resources\\output\\users-sold-products.xml";
+    public final static String CATEGORIES_BY_PRODUCTS_COUNT = "src\\main\\resources\\output\\categories-by-products.xml";
+    public final static String USERS_AND_PRODUCTS = "src\\main\\resources\\output\\users-and-products.xml";
+    public final static String ORDERED_CUSTOMERS = "src\\main\\resources\\output\\ordered-customers.xml";
+    public final static String TOYOTA_CARS = "src\\main\\resources\\output\\toyota-cars.xml";
+    public final static String LOCAL_SUPPLIERS = "src\\main\\resources\\output\\local-suppliers.xml";
+    public final static String CAR_AND_PARTS = "src\\main\\resources\\output\\cars-and-parts.xml";
+    public final static String CUSTOMERS_TOTAL_SALES = "src\\main\\resources\\output\\customers-total-sales.xml";
+    public final static String SALES_DISCOUNTS = "src\\main\\resources\\output\\sales-discounts.xml";
+}
---
+++ P17XMLProcessing-ProductShop-CarDealer/src/main/java/com/example/productshop/enums/ReadXmlPaths.java
@@ -0,0 +1,12 @@
+package com.example.productshop.enums;
+
+public class ReadXmlPaths {
+    ;
+    public final static String USERS_XML_PATH = "src\\main\\resources\\input\\users.xml";
+    public final static String CATEGORIES_XML_PATH = "src\\main\\resources\\input\\categories.xml";
+    public final static String PRODUCTS_XML_PATH = "src\\main\\resources\\input\\products.xml";
+    public final static String SUPPLIERS_XML_PATH = "src\\main\\resources\\input\\suppliers.xml";
+    public final static String PARTS_XML_PATH = "src\\main\\resources\\input\\parts.xml";
+    public final static String CARS_XML_PATH = "src\\main\\resources\\input\\cars.xml";
+    public final static String CUSTOMERS_XML_PATH = "src\\main\\resources\\input\\customers.xml";
+}
