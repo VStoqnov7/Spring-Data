1daac818ebd2333db751be1644a7a52080167cb1
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-05-10 00:34:48 +0300

    update

---
+++ P19Exams/P10-Exam-3-April-2020/src/main/java/softuni/exam/util/LocalDateTimeAdapter.java
@@ -0,0 +1,22 @@
+package softuni.exam.util;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public LocalDateTime unmarshal(String v) throws Exception {
+        return LocalDateTime.parse(v,formatter);
+    }
+
+    @Override
+    public String marshal(LocalDateTime v) throws Exception {
+        return v.format(formatter);
+    }
+}
+
+//@XmlJavaTypeAdapter(LocalDateTimeAdapter.class)
