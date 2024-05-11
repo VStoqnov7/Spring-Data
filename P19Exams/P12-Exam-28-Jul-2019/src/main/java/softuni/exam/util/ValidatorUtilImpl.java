9f3fec8660ab2440aaec3f5c6256e9198508ee1c
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-05-12 00:16:17 +0300

    update

---
+++ P19Exams/P12-Exam-28-Jul-2019/src/main/java/softuni/exam/util/ValidatorUtilImpl.java
@@ -0,0 +1,24 @@
+package softuni.exam.util;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+public class ValidatorUtilImpl implements ValidatorUtil {
+
+    private final Validator validator;
+
+    public ValidatorUtilImpl(Validator validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    public <E> boolean isValid(E entity) {
+        return this.validator.validate(entity).isEmpty();
+    }
+
+    @Override
+    public <E> Set<ConstraintViolation<E>> violations(E entity) {
+        return validator.validate(entity);
+    }
+}
